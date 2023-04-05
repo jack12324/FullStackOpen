cab445d59cab254e93ceba2c824e9813f728a301
Jack <jacksporter24@gmail.com>
2023-04-04 20:06:03 -0400

    1.7

--- part1/part1cd/src/App.js
+++ part1/part1cd/src/App.js
@@ -2,6 +2,22 @@ import {useState} from 'react'
 
 const Button = ({ handleClick, text }) => (  <button onClick={handleClick}>    {text}  </button>)
 const Statistic = ({name, count}) => (<p>{name} {count}</p>)
+const Percent = ({name, count}) => (<p>{name} {count} %</p>)
+
+const Statistics = ({good, neutral, bad}) => {
+    const total = good+bad+neutral
+    return(
+        <>
+            <Statistic name={'good'} count={good}/>
+            <Statistic name={'neutral'} count={neutral}/>
+            <Statistic name={'bad'} count={bad}/>
+            <Statistic name={'all'} count={total}/>
+            <Statistic name={'average'} count={(good-bad)/total}/>
+            <Percent name={'positive'} count={(good/total)*100}/>
+        </>
+    )
+}
+
 const App = () => {
     // save clicks of each button to its own state
     const [good, setGood] = useState(0)
@@ -17,9 +33,7 @@ const App = () => {
                 <Button handleClick={()=>setBad(bad + 1)} text={'bad'}/>
             </div>
             <h1>statistics</h1>
-            <Statistic name={'good'} count={good}/>
-            <Statistic name={'neutral'} count={neutral}/>
-            <Statistic name={'bad'} count={bad}/>
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
