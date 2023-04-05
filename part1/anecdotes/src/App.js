ef864e5db08b4b54e2416a5c3eac499329e6cdc2
Jack <jacksporter24@gmail.com>
2023-04-04 21:23:11 -0400

    1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -2,6 +2,14 @@ import { useState } from 'react'
 
 const Button = ({ handleClick, text }) => (  <button onClick={handleClick}>    {text}  </button>)
 
+const Anecdote = ({heading, anecdote, votes}) => (
+    <>
+      <h1>{heading}</h1>
+      <p>{anecdote}</p>
+      <p>has {votes} votes</p>
+    </>
+)
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -13,6 +21,7 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
   const [selected, setSelected] = useState(0)
   const getRandomSelected = () => {
@@ -21,10 +30,22 @@ const App = () => {
     setSelected(random)
   }
 
+  const incrementVote = (idx) => () => {
+    const new_votes = [...votes]
+    new_votes[idx] += 1
+    setVotes(new_votes)
+  }
+
+  const most_votes = votes.indexOf(Math.max(...votes))
+
   return (
       <div>
-        <p>{anecdotes[selected]}</p>
-        <Button text={'next anecdote'} handleClick={getRandomSelected}/>
+        <Anecdote anecdote={anecdotes[selected]} heading={'Anecdote of the day'} votes={votes[selected]}/>
+        <div>
+          <Button text={'vote'} handleClick={incrementVote(selected)}/>
+          <Button text={'next anecdote'} handleClick={getRandomSelected}/>
+        </div>
+        <Anecdote anecdote={anecdotes[most_votes]} heading={'Anecdote with most votes'} votes={votes[most_votes]}/>
       </div>
   )
 }
