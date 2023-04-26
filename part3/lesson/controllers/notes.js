a1998754de2c792826f49dc6fbbf280b6abb2966
Jack <jacksporter24@gmail.com>
2023-04-26 16:04:12 -0400

    Started adding users

--- part3/lesson/controllers/notes.js
+++ part3/lesson/controllers/notes.js
@@ -1,12 +1,21 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.startsWith('bearer ')){
+    return authorization.replace('bearer ', '')
+  }
+  return null
+}
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
   response.json(notes)
 })
 
 notesRouter.get('/:id', async (request, response ) => {
-  const note = await Note.findById(request.params.id)
+  const note = await Note.findById(request.params.id).populate('user', { username: 1, name: 1 })
   if (note) {
     response.json(note)
   } else {
@@ -21,13 +30,25 @@ notesRouter.delete('/:id', async (request, response) => {
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if(!decodedToken) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  console.log(decodedToken)
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
     important: body.important || false, //defaults value to false
+    user: user._id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
+  console.log(user)
+
   response.status(201).json(savedNote)
 
 })
